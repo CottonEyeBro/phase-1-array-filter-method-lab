b034a893a0cd8bb1a4dacf929d2f6af0aef8a67d
Cooper <55swimmer55@gmail.com>
2023-08-23 14:57:15 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+function findMatching(names, string) {
+    return names.filter((name) => {
+        if (name.toLowerCase() === string.toLowerCase()) {
+            return true;
+        }
+    })
+}
+
+function fuzzyMatch(names, string) {
+    return names.filter((name) => {
+        return (name.startsWith(string));
+    })
+}
+
+function matchName(drivers, string) {
+    return drivers.filter((driver) => {
+        return driver.name === string;
+    })
+}
